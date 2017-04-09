ea13804cd1a9261c994a31d19e2044897fd6f807
Leslie Barron <lesliebarron1@gmail.com>
2017-04-08 21:13:45 -0400

    added token

--- later.py
+++ later.py
@@ -2,8 +2,10 @@ from flask import Flask, request, g
 from flask_cors import CORS, cross_origin
 from flask_restful import abort, Resource, Api
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_httpauth import HTTPBasicAuth
+
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 
 from passlib.apps import custom_app_context as pwd_context
 
@@ -41,6 +43,22 @@ class User(db.Model):
     def as_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
+    def generate_auth_token(self, expiration = 600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({ 'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None # valid token, but expired
+        except BadSignature:
+            return None # invalid token
+        user = User.query.get(data['id'])
+        return user
+
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     owner = db.Column(db.Integer, db.ForeignKey("user.id"))
@@ -61,12 +79,22 @@ def abort_if_id_doesnt_exist(dic, id):
         abort(404, message="id {} doesn't exist".format(id))
 
 @auth.verify_password
-def verify_password(username, password):
-    user = User.query.filter_by(username = username).first()
-    if not user or not user.verify_password(password):
-        return False
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
     g.user = user
     return True
+
+@app.route('/login')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({ 'token': token.decode('ascii') })
 
 class UserAPI(Resource):
     def get(self, user_id):
